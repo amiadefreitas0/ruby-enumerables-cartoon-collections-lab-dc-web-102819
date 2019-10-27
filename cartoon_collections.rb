9ff32ab8016fdb64fb02ae27aa2390f666a0326f
Amia Defreitas <amia.defreitas0@gmail.com>
2019-10-27 17:19:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 def summon_captain_planet(veggies)# code an argument here
   # Your code here
   # planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-    veggies.collect do |call|
+    veggies.collect do |call| 
     call.capitalize + "!"
   end
 
@@ -31,6 +31,5 @@ def find_the_cheese(cheese)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese.find do |cheese|
     cheese_types.include?(cheese)
-
   end
 end
